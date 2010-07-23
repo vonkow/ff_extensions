439da66bffc14697cdcc7b956b6744701d59a631
Caz vonKow <skopsycats@gmail.com>
2010-07-23 15:59:17 -0400

    Chrome Passover working, kinda

--- chrome/passover/cont.js
+++ chrome/passover/cont.js
@@ -1,1 +1,26 @@
-//alert('hey');
+var licenses = [];
+var getAndStore = function(targ) {
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange=function() {
+		if (xhr.readyState==4 && xhr.status==200) {
+			licenses.push(JSON.parse(xhr.responseText));
+		}
+	}
+	xhr.open('GET', targ, true);
+	xhr.send(null);
+};
+var scripts = document.getElementsByTagName('script');
+for (var x=0; x<scripts.length; x++) {
+	if (scripts[x].getAttribute('data-license')) {
+		getAndStore(scripts[x].getAttribute('data-license'));
+	}
+};
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+  if (request.method == "getLicenses") {
+    // Send JSON data back to Popup.
+    sendResponse({data: licenses});
+  } else {
+    sendResponse({}); // snub them.
+  }
+});
+
